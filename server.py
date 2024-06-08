1b6d7639c2fb682db01d81235490924dc4678841
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-08 16:01:14 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -2,40 +2,82 @@
 # -*- coding: utf-8 -*-
 
 """
-About: Simple TCP socket server.
+About: Simple server for counting.
 """
 
+import argparse
+import signal
 import socket
+import time
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+INTERNAL_IP_H2 = "192.168.0.12"
+INTERNAL_IP_H3 = "192.168.0.13"
+INTERNAL_PORT = 9999
+SERVICE_IP = "10.0.0.12"
+SERVICE_PORT = 8888
+HOST_NAME = None
 
-# Bind the socket to the port
-server_address = ("", 65000)
-print("starting up on {} port {}".format(*server_address))
-sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+def recv_state(host_name):
+    """Get the latest counter state from the internal
+    network between h2 and h3.
+    """
+    if host_name == "h2":
+        recv_ip = INTERNAL_IP_H2
+    else:
+        recv_ip = INTERNAL_IP_H3
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((recv_ip, INTERNAL_PORT))
 
-while True:
-    # Wait for a connection
-    print("waiting for a connection")
-    connection, client_address = sock.accept()
-    try:
-        print("connection from", client_address)
+    state, _ = sock.recvfrom(1024)
+    state = int(state.decode("utf-8"))
+    return state
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print("received {!r}".format(data))
-            if data:
-                print("sending data back to the client")
-                connection.sendall(data)
-            else:
-                print("no data from", client_address)
-                break
 
-    finally:
-        # Clean up the connection
-        connection.close()
+def run(host_name, get_state=False):
+    """Run the couting service and handle sigterm signal."""
+    counter = 0
+    if get_state:
+        counter = recv_state(host_name)
+        print("Get the init counter state: {}".format(counter))
+
+    # Use closure to avoid using a global variable for state.
+    def term_signal_handler(signum, frame):
+        # Check if the server is running on the host 2.
+        if host_name == "h2":
+            dest_ip = INTERNAL_IP_H3
+        else:
+            dest_ip = INTERNAL_IP_H2
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Send duplicated packets to avoid losses.
+        for _ in range(6):
+            sock.sendto(str(counter).encode("utf-8"), (dest_ip, INTERNAL_PORT))
+        sock.close()
+
+    signal.signal(signal.SIGTERM, term_signal_handler)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((SERVICE_IP, SERVICE_PORT))
+
+    while True:
+        # Block here waiting for data input.
+        _, addr = sock.recvfrom(1024)
+        counter += 1
+        sock.sendto(str(counter).encode("utf-8"), addr)
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple counting server.")
+    parser.add_argument(
+        "hostname",
+        type=str,
+        help="The name of the host on which the server is deployed.",
+    )
+    parser.add_argument(
+        "--get_state", action="store_true", help="Get state from network."
+    )
+
+    args = parser.parse_args()
+
+    run(args.hostname, args.get_state)
