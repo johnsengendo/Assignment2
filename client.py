4d65dcac47ac264df22b57eef0b95f48029068a6
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-09 08:56:38 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,22 +1,37 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-About: Simple client.
-"""
-
 import socket
 import time
+import cv2
 
 SERVICE_IP = "10.0.0.12"
 SERVICE_PORT = 8888
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    data = b"Show me the counter, please!"
+
+    # Set up a video capture object to receive and decode the H.264 video stream
+    cap = cv2.VideoCapture()
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"H264"))
+
+    # Print a message to indicate that the video is streaming
+    print("Video is streaming...")
 
     while True:
-        sock.sendto(data, (SERVICE_IP, SERVICE_PORT))
-        counter, _ = sock.recvfrom(1024)
-        print("Current counter: {}".format(counter.decode("utf-8")))
-        time.sleep(1)
+        # Receive the next video frame from the server
+        data, _ = sock.recvfrom(1024 * 1024)
+
+        # Decode the H.264 video frame and display it
+        ret, frame = cap.decode(data)
+        if ret:
+            cv2.imshow("Video", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        # Sleep for a short time to prevent the client from using too much CPU time
+        time.sleep(1 / 30)
+
+    # Release the video capture object and close the socket
+    cap.release()
+    sock.close()
+
+    # Print a message to indicate that the video has stopped
+    print("Video has stopped.")
